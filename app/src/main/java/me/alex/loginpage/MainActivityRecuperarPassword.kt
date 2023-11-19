f5d810e509993e1a2b7c18a97739d11fec311b96
Evelyn Alejandra Cebada Cortés <acebadaa24@gmail.com>
2023-11-18 22:05:54 -0600

    Initial commit

---
+++ app/src/main/java/me/alex/loginpage/MainActivityRecuperarPassword.kt
@@ -0,0 +1,11 @@
+package me.alex.loginpage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityRecuperarPassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_recuperar_password)
+    }
+}
