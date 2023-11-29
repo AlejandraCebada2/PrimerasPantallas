89d2870448245cbdbb04e545a6e81b32d5a765e5
Evelyn Alejandra Cebada Cortés <acebadaa24@gmail.com>
2023-11-28 23:27:37 -0600

    NuevasVistas

---
+++ app/src/main/java/me/alex/loginpage/MainActivityPaginaPrincipal.kt
@@ -0,0 +1,11 @@
+package me.alex.loginpage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityPaginaPrincipal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_pagina_principal)
+    }
+}
